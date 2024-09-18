2338eab65fa5e11417c24602d177e5c7f3b36cda
Ali <wvkashif@gmail.com>
2024-09-18 01:45:43 -0700

    initial message

--- coding-challenge/backend/src/index.ts
+++ coding-challenge/backend/src/index.ts
@@ -16,3 +16,8 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+//initial request
+app.get("/", (req, res) => {
+  res.send("Welcome to the API!");
+});
